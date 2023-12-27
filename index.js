668c22cfd49e047750ef6fb5cc60ea863a17eb45
Casper Schut <c.schut@pm.me>
2023-12-27 14:10:38 +0100

    Working index.js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 require("dotenv").config();
-const Discord = require("discord.js");
-const client = new Discord.Client({
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const client = new Client({
 	intents: [
-		Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.MessageContent,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
 	],
 });
 
-client.on("ready", () => {
+client.once(Events.ClientReady, () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("message", (msg) => {
-	if (msg.content === "ping") {
-		msg.reply("pong");
-	}
 });
 
 client.login(process.env.TOKEN);
