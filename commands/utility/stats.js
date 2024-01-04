f3c97cb58608a6fee19a125d05fa27ea0394b4ec
Casper Schut <c.schut@pm.me>
2024-01-04 11:11:10 +0100

    created embed message

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -7,8 +7,16 @@ module.exports = {
 			"Presents the stats of this user. Adds a new user, if this command is used for the first time.",
 		),
 	async execute(interaction) {
-		const { codeBlock } = require("../../functions/codeBlock");
-		const msg = codeBlock("fix", "Hallo");
-		await interaction.reply(msg);
+		const { EmbedBuilder } = require("discord.js");
+		const userAvatar = interaction.user.displayAvatarURL();
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x78c0df)
+			.setTitle("Title")
+			.setDescription("Description")
+			.setThumbnail(userAvatar)
+			.addFields(
+				{ name: "Title", value: "Content" },
+			);
+		interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
